ad3713da41190f311e6ef1e19af001e2e055984d
Phil Wilkinson <phil@philwilky.me>
2023-05-24 17:19:10 +0100

    working cannonball (cball.cs) target
    using code i wrote in Unity junior programmer unit 4. I now need to figure out which enemy is the closest rather than just looking at any gameobject called enemy. And not destroy everything it touches.

---
+++ Assets/Scripts/cball.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cball : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public Rigidbody selfRb;
+    public GameObject enemyObj;
+
+    void Start()
+    {
+        selfRb = GetComponent<Rigidbody>(); //find the rigidbody component on the same object the script is on
+        enemyObj = GameObject.Find("Enemy"); // finds the player game object by looking for its inspector name
+    }
+
+    void Update()
+    {
+        if (enemyObj != null)
+        {
+            Vector3 lookDirection = (enemyObj.transform.position - transform.position).normalized;
+
+            // enemy chases player object
+            selfRb.AddForce(lookDirection * speed);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(collision.gameObject);
+    }
+
+}
